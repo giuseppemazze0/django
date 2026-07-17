aa2d9a9b903d39b73a6b536259d7db0c55124773
giuseppemazze0 <a22204542@alunos.ulht.pt>
2026-07-17 08:54:58 +0000

    atualizacao da pagina faculdade

--- portfolio/static/portfolio/scripts.js
+++ portfolio/static/portfolio/scripts.js
@@ -20,3 +20,54 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
         smoother.scrollTo(target, true);
     });
 });
+
+
+
+
+// Faculdade
+
+gsap.from('.menu-cards > .card', {
+    opacity: 0,
+    y: 40,
+    stagger: .3,
+    delay: .4,
+});
+
+
+gsap.from('.destaque', {
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#quem-somos",
+        start: "top 20%",
+    }
+});
+
+
+const cards = gsap.utils.toArray(".card-legal");
+let delay = .1;
+
+for (let card of cards) {
+    gsap.from(card, {
+        opacity: 0,
+        y: 40,
+        delay: delay,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: "#licenciatura",
+            start: "top 50%",
+            markers: true
+        }
+    });
+    delay += .1;
+}
+
+gsap.from("#licenciatura h3", {
+    opacity: 0,
+    x: -40,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: "#licenciatura",
+        start: "top 50%",
+        markers: true
+    }
+});
--- project/settings.py
+++ project/settings.py
@@ -87,6 +87,9 @@ STORAGES = {
     },
 }
 
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
@@ -102,6 +105,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
 
 
 
@@ -171,4 +176,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+
